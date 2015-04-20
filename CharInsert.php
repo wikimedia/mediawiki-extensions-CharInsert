1f6214643cfafb22de8daed0a5f4fd07ec9582ae
paladox <thomasmulhall410@yahoo.com>
2015-04-20 17:40:58 +0200

    empty PHP entry point
    Change-Id: Ia2d5ebe5693d8c9b274606a19395ff6fda1f9ebc

--- CharInsert.php
+++ CharInsert.php
@@ -1,51 +1,13 @@
 <?php
-/**
- * Extension to create new character inserts which can be used on
- * the edit page to make it easy to get at special characters and
- * such forth.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @file
- * @ingroup Extensions
- * @author Brion Vibber <brion at pobox.com>
- * @copyright Copyright (C) 2004,2006 Brion Vibber <brion@pobox.com>
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die();
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'CharInsert' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['CharInsert'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for FooBar extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the CharInsert extension requires MediaWiki 1.25+' );
 }
-
-/**
- * This PHP entry point is deprecated. Please use wfLoadExtension() and the extension.json file instead.
- * See https://www.mediawiki.org/wiki/Manual:Extension_registration for more details.
- */
-
-$wgHooks['ParserFirstCallInit'][] = 'CharInsert::onParserFirstCallInit';
-
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'CharInsert',
-	'author' => 'Brion Vibber',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:CharInsert',
-	'descriptionmsg' => 'charinsert-desc',
-	'license-name' => 'GPL-2.0+',
-);
-
-$wgAutoloadClasses['CharInsert'] = __DIR__ . '/CharInsert.body.php';
-$wgMessagesDirs['CharInsert'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['CharInsert'] = __DIR__ . '/CharInsert.i18n.php';
