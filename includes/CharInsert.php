18ad8a255cd2f60193eae98b6799e96f45930afa
tacsipacsi <tacsipacsi@jnet.hu>
2021-12-20 22:39:10 +0100

    Allow overriding the link text
    Displaying the character to be inserted is perfect in most cases, but
    some obscure characters may require explanation or very long snippets
    like templates a shorter label.
    
    This patch allows setting a different label using the `label` XML-style
    attribute. This attribute is technically usable with charinsert tags
    containing multiple inserts, but it shouldn’t be used in that way: it
    results in multiple links with the exact same text, so the user has no
    way to distinguish them before clicking. (I couldn’t find any obvious
    way to sytnactically prevent this, if anyone has an idea, I’m open to
    it.)
    
    To easily pass the attribute to where it’s used, the CharInsert class
    is now instantiable, and the attributes are passed in the constructor.
    While I was there, I also changed these methods to private and renamed
    them to not have fully-qualified names, as they’re qualified by the
    class name anyway.
    
    There are two new parser test cases: one for the new functionality, and
    one for the already-existing functionality involving the `+` character.
    The latter describes how it actually works, which may not be how it was
    planned to work, but I didn’t want to change the behavior in this patch.
    
    Bug: T25354
    Change-Id: I70d282e61d92fe8d82afa3d2443add6e36956dc1

--- includes/CharInsert.php
+++ includes/CharInsert.php
@@ -7,22 +7,65 @@ use Sanitizer;
 use Xml;
 
 class CharInsert {
-	public static function charInsertHook( $data, $params, Parser $parser ) {
-		$data = $parser->getStripState()->unstripBoth( $data );
-		$parser->getOutput()->addModules( [ 'ext.charinsert' ] );
-		$parser->getOutput()->addModuleStyles( [ 'ext.charinsert.styles' ] );
+	/** @var array XML-style attributes passed to the tag */
+	private $params;
+	/** @var Parser */
+	private $parser;
+
+	/**
+	 * Main entry point, called by the parser.
+	 * @param string $data The textual content of the <charinsert> tag
+	 * @param array $params XML-style attributes of the <charinsert> tag
+	 * @param Parser $parser
+	 * @return string
+	 */
+	public static function charInsertHook( $data, array $params, Parser $parser ): string {
+		return ( new self( $params, $parser ) )->expand( $data );
+	}
+
+	/**
+	 * Constructor.
+	 * @param array $params XML-style attributes of the <charinsert> tag
+	 * @param Parser $parser
+	 */
+	private function __construct( array $params, Parser $parser ) {
+		$this->params = $params;
+		$this->parser = $parser;
+	}
+
+	/**
+	 * Parse the content of a whole <charinsert> tag.
+	 * @param string $data The textual content of the <charinsert> tag
+	 * @return string HTML to be inserted in the parser output
+	 */
+	private function expand( $data ): string {
+		$data = $this->parser->getStripState()->unstripBoth( $data );
+		$this->parser->getOutput()->addModules( [ 'ext.charinsert' ] );
+		$this->parser->getOutput()->addModuleStyles( [ 'ext.charinsert.styles' ] );
 		return implode( "<br />\n",
-			array_map( [ self::class, 'charInsertLine' ],
+			array_map( [ $this, 'processLine' ],
 				explode( "\n", trim( $data ) ) ) );
 	}
 
-	public static function charInsertLine( $data ) {
+	/**
+	 * Parse a single line in the <charinsert> tag.
+	 * @param string $data Textual content of the line
+	 * @return string HTML to be inserted in the parser output
+	 */
+	private function processLine( string $data ): string {
 		return implode( "\n",
-			array_map( [ self::class, 'charInsertItem' ],
-				preg_split( '/\s+/', self::charInsertArmor( $data ) ) ) );
+			array_map( [ $this, 'processItem' ],
+				preg_split( '/\s+/', $this->armor( $data ) ) ) );
 	}
 
-	public static function charInsertArmor( $data ) {
+	/**
+	 * Escape literal whitespace characters in <nowiki> tags. Whitespace
+	 * within <nowiki> tags is not considered to be an insert boundary.
+	 * @param string $data Textual content of the line to escape whitespace in
+	 * @return string The textual content with whitespace escaped and <nowiki>
+	 *  tags removed
+	 */
+	private function armor( string $data ): string {
 		return preg_replace_callback(
 			'!<nowiki>(.*?)</nowiki>!i',
 			static function ( array $matches ) {
@@ -36,21 +79,35 @@ class CharInsert {
 		);
 	}
 
-	public static function charInsertItem( $data ) {
+	/**
+	 * Parse a single insert, i.e. largest portion of the input that
+	 * (after going through armor()) doesn’t contain ASCII whitespace.
+	 * @param string $data The single insert.
+	 * @return string HTML to be inserted in the parser output
+	 */
+	private function processItem( string $data ): string {
 		$chars = explode( '+', $data );
 		if ( count( $chars ) > 1 && $chars[0] !== '' ) {
-			return self::charInsertChar( $chars[0], $chars[1] );
+			return $this->insertChar( $chars[0], $chars[1] );
 		} elseif ( count( $chars ) === 1 ) {
-			return self::charInsertChar( $chars[0] );
+			return $this->insertChar( $chars[0] );
 		} else {
-			return self::charInsertChar( '+' );
+			return $this->insertChar( '+' );
 		}
 	}
 
-	public static function charInsertChar( $start, $end = '' ) {
-		$estart = self::charInsertDisplay( $start );
-		$eend = self::charInsertDisplay( $end );
-		$inline = $estart . $eend;
+	/**
+	 * Create the HTML for a single insert.
+	 * @param string $start The start part of the insert, inserted before
+	 *  the text selected by the user (if there is any).
+	 * @param string $end The end part of the insert, inserted after
+	 *  the text selected by the user (if there is any).
+	 * @return string HTML to be inserted in the parser output
+	 */
+	private function insertChar( string $start, string $end = '' ): string {
+		$estart = $this->getInsertAttribute( $start );
+		$eend = $this->getInsertAttribute( $end );
+		$inline = $this->params['label'] ?? ( $estart . $eend );
 
 		// Having no href attribute makes the link be more
 		// easily copy and pasteable for non-js users.
@@ -63,7 +120,13 @@ class CharInsert {
 		);
 	}
 
-	public static function charInsertDisplay( $text ) {
+	/**
+	 * Double-escape non-breaking spaces. This allows the user to
+	 * differentiate the user normal and non-breaking spaces.
+	 * @param string $text The text to double-escape NBSPs in
+	 * @return string
+	 */
+	private function getInsertAttribute( string $text ): string {
 		static $invisibles = [ '&nbsp;', '&#160;' ];
 		static $visibles = [ '&amp;nbsp;', '&amp;#160;' ];
 		return Sanitizer::decodeCharReferences(
